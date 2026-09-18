bbc46eccddeb6041d0a0185d44faf3c0b68a73a0
Gérard Smal <g.smal@betacom.it>
2026-09-18 15:57:38 +0200

    Allow localhost CORS origin

--- src/main/java/com/betacom/ve/configuration/SecurityConfig.java
+++ src/main/java/com/betacom/ve/configuration/SecurityConfig.java
@@ -63,7 +63,7 @@ public class SecurityConfig {
     CorsConfigurationSource corsConfigurationSource() {
         CorsConfiguration config = new CorsConfiguration();
 
-        config.setAllowedOrigins(List.of("http://localhost:4200"));
+        config.setAllowedOrigins(List.of("http://localhost:4200", "http://localhost"));
         config.setAllowedMethods(List.of("GET", "POST", "PUT", "DELETE", "PATCH", "OPTIONS"));
         config.setAllowedHeaders(List.of("*"));
         config.setAllowCredentials(true);
